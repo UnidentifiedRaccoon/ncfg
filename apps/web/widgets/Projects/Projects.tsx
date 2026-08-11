03253a181b94bcc323a8f9aaf89339e388df2196
yura-posledov <yura.posledov@yandex.ru>
2026-08-11 21:19:50 +0300

    feat(web): add theater project to homepage (#32)

--- apps/web/widgets/Projects/Projects.tsx
+++ apps/web/widgets/Projects/Projects.tsx
@@ -23,15 +23,9 @@ export function Projects({
 }: ProjectsProps) {
   return (
     <Section id="projects" title={title} background="gray">
-      <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 lg:grid-cols-6">
-        {projects.map((project, index) => (
-          <li
-            key={project.id}
-            className={cn(
-              "h-full",
-              index < 2 ? "lg:col-span-3" : "lg:col-span-2"
-            )}
-          >
+      <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 lg:grid-cols-3">
+        {projects.map((project) => (
+          <li key={project.id} className="h-full">
             <a
               href={project.href}
               target="_blank"
